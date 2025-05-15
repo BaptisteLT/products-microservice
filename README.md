459ef1ceea38884fc49278473d67b485477014c7
Baptiste <lantranbaptiste@gmail.com>
2025-05-15 14:36:15 +0200

    Initial commit avec api API PLATFORM

---
+++ README.md
@@ -0,0 +1,22 @@
+Pour build le Dockerfile:
+docker-compose build
+
+Pour lancer les différents conteneurs: 
+docker-compose up -d
+
+Pour arrêter docker :
+docker-compose stop
+
+Pour supprimer les containers :
+docker-compose down
+
+
+Pour supprimer les containers avec les volumes :
+docker-compose down --volumes
+
+Pour accéder en local:
+Symfony : http://127.0.0.1:8082
+PhpMyAdmin : http://127.0.0.1:8083
+
+Pour éxec les commandes symfony:
+docker exec -it php bash
---
+++ products-microservice/src/Entity/Product.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+#[ApiResource]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $priceInCents = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $color = null;
+
+    #[ORM\Column]
+    private ?int $stock = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPriceInCents(): ?int
+    {
+        return $this->priceInCents;
+    }
+
+    public function setPriceInCents(int $priceInCents): static
+    {
+        $this->priceInCents = $priceInCents;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(?string $color): static
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(int $stock): static
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+}
